8ce302b5ef18644fbdece30d08c6bd8e50b0dd11
Ayan-IPIC <155599505+Ayan-IPIC@users.noreply.github.com>
2025-06-13 16:19:32 +0800

    Update get_products.py

--- examples/get_products.py
+++ examples/get_products.py
@@ -1,7 +1,7 @@
 from veeqo import VeeqoClient
 import os
 
-API_KEY = os.getenv("VEEQO_API_KEY", "Vqt/800967642dae577f3ed97a48ca8f9ff0")
+API_KEY = os.getenv("VEEQO_API_KEY", "****")
 
 client = VeeqoClient(api_key=API_KEY)
 products = client.list_products(page=1)
